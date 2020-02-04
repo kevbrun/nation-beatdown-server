4a25c204dabe839182526a15dd0b7e6de4e7f3c2
Kevin Brunner <kevin.rene.brunner@gmail.com>
2020-02-04 18:13:00 +0100

    added add. config to web gate filter2

--- zuul/src/main/java/ch/nation/zuul/security/GatewayConfiguration.java
+++ zuul/src/main/java/ch/nation/zuul/security/GatewayConfiguration.java
@@ -12,10 +12,32 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(final HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/oauth/**","/api/rest/v1/oauth/**","/**/oauth/**")
+                .antMatchers("/api/rest/v1/oauth/**")
                 .permitAll()
+                //For registering the user
                 .antMatchers(HttpMethod.POST,"/api/rest/v1/users").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/users/search/identifier**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/classes/search/identifier**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/skills/search/identifier**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/prejudices/search/identifier**").permitAll()
+
+                //Allow get All points to be accessed for user registration part
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/characteristics**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/classes**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/skills**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/prejudices**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/units/full-name/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/units/first-name/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/rest/v1/units/last-name/**").permitAll()
+
+
+
+                //Endpoints used for registering user
+
                 .antMatchers("/**")
                 .authenticated();
+
+
+        //TODO CHECK IF OK WITH AUTH SERVICE
     }
 }
